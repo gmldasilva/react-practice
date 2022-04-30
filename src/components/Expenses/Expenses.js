ba29320a54d49e39d37e6a595c6e3d898ac144a2
Gabriel Marcondes <gmldasilva@gmail.com>
2022-04-30 19:00:08 -0300

    Finishing section 3

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,23 @@
+import './Expenses.css';
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+ 
+function Expenses(props) {
+ 
+    const expenses = props.expenses.map( (expense) => {
+        return (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />);
+    });
+ 
+    return (
+    <Card className="expenses">
+        {expenses}
+    </Card>)
+}
+ 
+export default Expenses;
---
+++ src/components/UI/Card.js
@@ -0,0 +1,8 @@
+import './Card.css';
+
+function Card(props) { // Wrapper Component
+    const classes = 'card ' + props.className;
+    return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
