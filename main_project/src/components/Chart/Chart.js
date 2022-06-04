4c61928e3bbbca38ae209170b9cd3d0be166f79f
Gabriel Marcondes <gmldasilva@gmail.com>
2022-06-04 10:27:51 -0300

    Finished expenses-tracker

--- main_project/src/components/Chart/Chart.js
+++ main_project/src/components/Chart/Chart.js
@@ -4,8 +4,11 @@ import ChartBar from './ChartBar';
 import './Chart.css'
 
 const Chart = props => {
+    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+    const totalMaximum = Math.max(...dataPointValues);
+
     return <div className="chart">
-        {props.dataPoints.map(dataPoint => <ChartBar value={dataPoint.value} maxValue  />)}
+        {props.dataPoints.map(dataPoint => <ChartBar key={dataPoint.id} value={dataPoint.value} maxValue={totalMaximum} label={dataPoint.label} />)}
     </div>
 };
 
--- main_project/src/components/Expenses/Expenses.js
+++ main_project/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import './Expenses.css';
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
  
 function Expenses(props) {
     const [filteredYear, setFilteredYear] = useState("2020")
@@ -17,6 +18,7 @@ function Expenses(props) {
     return (
         <Card className="expenses">
             <ExpenseFilter currentSelectedYear={filteredYear} onChangeFilter={filterChangeHandler}/>
+            <ExpensesChart expenses={filteredExpenses}/>
             <ExpensesList items={filteredExpenses}/>
         </Card>)
 }
