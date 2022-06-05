8bdd209d7684eaa6dd0c54a0784c09c19e1e52f9
Gabriel Marcondes <gmldasilva@gmail.com>
2022-06-05 18:44:51 -0300

    Using portals for modals rendering

--- user_crud/src/components/Users/AddUser.js
+++ user_crud/src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from "../UI/ErrorModal";
 import styles from "./AddUser.module.css";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
     const [formValues, setFormValues] = useState({username:'', age: ''});
@@ -40,7 +41,7 @@ const AddUser = (props) => {
     }
 
     return (
-        <div>
+        <Wrapper>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
             <Card className={styles.input}>
                 <form onSubmit={addUserHandler}> 
@@ -52,7 +53,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
     );
 };
 
